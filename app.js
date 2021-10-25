58e3ebdf7e0d4b722022d9a2d40cee346fb4f2df
nithinag <nithinag10@gmail.com>
2021-10-25 14:20:03 +0530

    handeled url not found

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
-const tasks = require('./routes/task')
-const conenctionDb = require('./db/connect')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const router = express.Router();
+const tasks = require("./routes/task");
+const conenctionDb = require("./db/connect");
+require("dotenv").config();
+const notFoundHandling = require("./middleware/notFoundHandling");
 
 //middleware
-app.use(express.json())
+app.use(express.static("./assets"));
+app.use(express.json());
 
 //routes
-app.use('/' , tasks)
+app.use("/api/taskmaster/", tasks);
 
-const port = 3000
+//Handling Not Found
+app.use(notFoundHandling);
+
+const port = 3000;
 
 const start = async () => {
   try {
-    await conenctionDb(process.env.MONGO_URI)
-    app.listen(port , console.log(`Server listning in port ${port}...`))
+    await conenctionDb(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server listning in port ${port}...`));
+  } catch (err) {
+    console.log(`Error in connecting to Database ${err}`);
   }
-  catch (err) {
-  console.log(`Error in connecting to Database ${err}`)
-  }
-}
+};
 
-start()
-
+start();
